658cd55a98ba3bb8151c399f2ee97563d9f33197
shipra100 <luckymaddhesiya344@gmail.com>
2024-05-09 14:41:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import AgeCal from './components/AgeCal';
+
+function App() {
+  return (
+    <div className="App">
+     <AgeCal />
+    </div>
+  );
+}
+
+export default App;
